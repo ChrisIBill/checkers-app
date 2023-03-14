aa91359bb4d5b6cd106e3a7921742bddc4287e1e
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-03-14 17:05:14 -0500

    ...

--- express-server/src/sockets/authHandler.ts
+++ express-server/src/sockets/authHandler.ts
@@ -3,5 +3,8 @@ import Paths from "../routes/constants/Paths";
 export function handleAuthorization(io: Server) {
     io.of(Paths.Auth.Login).on("connection", (socket) => {
         console.log("User connected with Auth Server", socket.id);
+        socket.on("UserLogin", (...args: any[]) => {
+            console.log(args);
+        });
     });
 }
