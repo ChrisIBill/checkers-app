7265ecd6d29f453bf3f75ac191c0473796a60f98
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-04-07 21:40:55 -0500

    Client: Interface and Consts work

--- client/src/constants/SessionConsts.ts
+++ client/src/constants/SessionConsts.ts
@@ -3,4 +3,5 @@ import {ISessionContext} from "../interfaces/SessionInterfaces";
 export const DEFAULT_SESSION_DATA: ISessionContext = {
 	userData: null,
 	isOnline: false,
+	socket: null,
 };
--- client/src/interfaces/RoomInterfaces.ts
+++ client/src/interfaces/RoomInterfaces.ts
@@ -4,3 +4,27 @@ export type RoomTypes = typeof ROOM_TYPES[keyof typeof ROOM_TYPES];
 export type CheckersRoomStylesTypes =
 	typeof CHECKERS_ROOM_STYLES[keyof typeof CHECKERS_ROOM_STYLES];
 export type AllRoomStylesTypes = CheckersRoomStylesTypes;
+
+export interface INewRoomState {
+	roomType: RoomTypes;
+	roomStyle: any;
+}
+export interface IActiveRoomState {
+	roomType: RoomTypes;
+	roomID: string;
+}
+export const DEFAULT_ROOM_STATUS = [
+	"new",
+	"loading",
+	"active",
+	"waiting",
+	"error",
+] as const;
+export type RoomStatusTypes = typeof DEFAULT_ROOM_STATUS[number];
+/* export interface ICheckersRoomData {
+
+export type RoomDataTypes = ICheckersRoomData | IChatRoomData; */
+export interface IRoomData {
+	status: RoomStatusTypes;
+	data: any;
+}
